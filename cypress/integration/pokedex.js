bb583dc1c742cae8fdccefda9a97b1cb6604bcf2
Mike Vedsted <mikevedsted@gmail.com>
2021-03-05 14:15:15 +0200

    Add exercise 11.9 step 2

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -4,4 +4,10 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate from front page to a particular pokemon', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('wailord').click()
+    cy.contains('water veil')
+  })
 })
